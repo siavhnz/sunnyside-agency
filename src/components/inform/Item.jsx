00a3b3759256c95bb369ce9e6aa89c9e7537fd7a
siavhnz <siavhnz@gmail.com>
2023-06-08 19:48:31 +0330

    fix inform component images

--- src/components/inform/Item.jsx
+++ src/components/inform/Item.jsx
@@ -13,8 +13,8 @@ const InfromItem = ({
           direction === "left" ? "justify-self-start" : "justify-self-end"
         }`}
       >
-        <source media="(max-width:750px)" srcSet={mobileImg} />
-        <source media="(min-width:751px)" srcSet={desktopImg} />
+        <source media="(max-width:499px)" srcSet={mobileImg} />
+        <source media="(min-width:500px)" srcSet={desktopImg} />
         <img src={mobileImg} alt={title} className="sm:w-full md:w-auto" />
       </picture>
       <div className="col-span-full row-span-full justify-self-center self-end pb-14 px-4 max-w-sm">
